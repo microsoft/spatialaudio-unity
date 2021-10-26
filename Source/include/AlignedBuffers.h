11c6fe23de31c8fca60ef8bc2add038bdf276b97
ashtat <ashtat@microsoft.com>
2021-10-26 10:56:49 -0700

    Address PR comments

--- Source/include/AlignedBuffers.h
+++ Source/include/AlignedBuffers.h
@@ -1,4 +1,5 @@
-/* Copyright (c) Microsoft Corp. */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 #pragma once
 #include "AlignedAllocator.h"
 #include "ConstVector.h"
--- Source/include/WaveFormat.h
+++ Source/include/WaveFormat.h
@@ -1,4 +1,5 @@
-// Copyright (c) Microsoft Corp. All rights reserved.
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.
 #pragma once
 
 #include <cstdint>
